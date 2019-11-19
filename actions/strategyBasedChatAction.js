14bbfd1931c0ddd33a00bc133dfc8b8d17f67899
Aaron Tainter <atainter@iastate.edu>
2019-11-18 18:35:34 -0800

    Add author details to youtube and twitch streams

--- actions/strategyBasedChatAction.js
+++ actions/strategyBasedChatAction.js
@@ -1,43 +1,43 @@
-const ChatActionInterface = require("./chatActionInterface");
+const ChatActionInterface = require("./chatActionInterface")
 
 class AbstractMessageStrategy {
-  matches(message, ctx) {
-    return true;
-  }
-
-  async getMessage(message, ctx) {
-    if (this.matches(message, ctx)) {
-      return this.makeMessage(message, ctx);
+    matches(message, ctx) {
+        return true
     }
-  }
 
-  async makeMessage(message, ctx) {
-    throw new Error("Unimplemented");
-  }
+    async getMessage(message, ctx) {
+        if (this.matches(message, ctx)) {
+            return this.makeMessage(message, ctx)
+        }
+    }
+
+    async makeMessage(message, ctx) {
+        throw new Error("Unimplemented")
+    }
 }
 
 class AbstractStrategyBasedChatAction extends ChatActionInterface {
-  constructor() {
-    super()
-    this.strategies = this.registerStrategies();
-  }
-
-  registerStrategies() {
-    return [];
-  }
-
-  async getMessage(message, ctx) {
-    for (const strategy of this.strategies) {
-      const strategyMessage = await strategy.getMessage(message, ctx);
-
-      if (strategyMessage) {
-        return strategyMessage;
-      }
+    constructor() {
+        super()
+        this.strategies = this.registerStrategies()
     }
-  }
+
+    registerStrategies() {
+        return []
+    }
+
+    async getMessage(message, ctx) {
+        for (const strategy of this.strategies) {
+            const strategyMessage = await strategy.getMessage(message, ctx)
+
+            if (strategyMessage) {
+                return strategyMessage
+            }
+        }
+    }
 }
 
 module.exports = {
-  AbstractMessageStrategy,
-  AbstractStrategyBasedChatAction
-};
+    AbstractMessageStrategy,
+    AbstractStrategyBasedChatAction
+}
